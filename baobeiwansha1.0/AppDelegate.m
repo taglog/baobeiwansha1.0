63f2e42b039fc74e7dfe4f1a7b6dccfe2caefba9
PanYongfeng <pan_yong_feng@hotmail.com>
2015-03-25 20:40:37 +0800

    no message

--- baobeiwansha1.0/AppDelegate.m
+++ baobeiwansha1.0/AppDelegate.m
@@ -15,6 +15,16 @@
 #import "ProfilePageViewController.h"
 #import "Reachability.h"
 
+// umeng 渠道分析所需头文件
+//for mac
+#include <sys/socket.h>
+#include <sys/sysctl.h>
+#include <net/if.h>
+#include <net/if_dl.h>
+//for idfa
+#import <AdSupport/AdSupport.h>
+
+
 @interface AppDelegate ()
 @property (nonatomic,retain) UINavigationController *userInfoNav;
 @property (nonatomic,retain) UITabBarController *mainTabBarController;
@@ -35,7 +45,7 @@
     [self initReachability];
     [self globalSettings];
     [self generateUserID];
-    [self pushNotificationSettings:launchOptions];
+    [self thirdPartiesCode:launchOptions];
     
     
 
@@ -138,14 +148,27 @@
     
 }
 
--(void)pushNotificationSettings:(NSDictionary *)launchOptions{
+-(void)thirdPartiesCode:(NSDictionary *)launchOptions{
 
     //push notification setting
     [AVOSCloud setApplicationId:@"zul2tbfbwbfhtzka27mea6ozakqg3m86v2dpk349e7hh9syv"
                       clientKey:@"0mikvyvihrejfctvqarlhwvuet67pahni8fjvrse8sai4okj"];
     [AVAnalytics trackAppOpenedWithLaunchOptions:launchOptions];
     
+    // umeng 渠道分析
+    NSString * appKey = @"5487dc8ffd98c53799000ea9";
+    NSString * deviceName = [[[UIDevice currentDevice] name] stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+    NSString * mac = [self macString];
+    NSString * idfa = [self idfaString];
+    NSString * idfv = [self idfvString];
+    NSString * urlString = [NSString stringWithFormat:@"http://log.umtrack.com/ping/%@/?devicename=%@&mac=%@&idfa=%@&idfv=%@", appKey, deviceName, mac, idfa, idfv];
+    [NSURLConnection connectionWithRequest:[NSURLRequest requestWithURL: [NSURL URLWithString:urlString]] delegate:nil];
+    
+    
+
+    
 }
+
 
 
 -(void)initViewControllers{
@@ -399,6 +422,107 @@
     }
     return string;
 }
+
+
+
+// umeng 渠道分析代码 begin
+
+- (NSString * )macString{
+    int mib[6];
+    size_t len;
+    char *buf;
+    unsigned char *ptr;
+    struct if_msghdr *ifm;
+    struct sockaddr_dl *sdl;
+    
+    mib[0] = CTL_NET;
+    mib[1] = AF_ROUTE;
+    mib[2] = 0;
+    mib[3] = AF_LINK;
+    mib[4] = NET_RT_IFLIST;
+    
+    if ((mib[5] = if_nametoindex("en0")) == 0) {
+        printf("Error: if_nametoindex error\n");
+        return NULL;
+    }
+    
+    if (sysctl(mib, 6, NULL, &len, NULL, 0) < 0) {
+        printf("Error: sysctl, take 1\n");
+        return NULL;
+    }
+    
+    if ((buf = malloc(len)) == NULL) {
+        printf("Could not allocate memory. error!\n");
+        return NULL;
+    }
+    
+    if (sysctl(mib, 6, buf, &len, NULL, 0) < 0) {
+        printf("Error: sysctl, take 2");
+        free(buf);
+        return NULL;
+    }
+    
+    ifm = (struct if_msghdr *)buf;
+    sdl = (struct sockaddr_dl *)(ifm + 1);
+    ptr = (unsigned char *)LLADDR(sdl);
+    NSString *macString = [NSString stringWithFormat:@"%02X:%02X:%02X:%02X:%02X:%02X",
+                           *ptr, *(ptr+1), *(ptr+2), *(ptr+3), *(ptr+4), *(ptr+5)];
+    free(buf);
+    
+    return macString;
+}
+
+- (NSString *)idfaString {
+    
+    NSBundle *adSupportBundle = [NSBundle bundleWithPath:@"/System/Library/Frameworks/AdSupport.framework"];
+    [adSupportBundle load];
+    
+    if (adSupportBundle == nil) {
+        return @"";
+    }
+    else{
+        
+        Class asIdentifierMClass = NSClassFromString(@"ASIdentifierManager");
+        
+        if(asIdentifierMClass == nil){
+            return @"";
+        }
+        else{
+            
+            //for no arc
+            //ASIdentifierManager *asIM = [[[asIdentifierMClass alloc] init] autorelease];
+            //for arc
+            ASIdentifierManager *asIM = [[asIdentifierMClass alloc] init];
+            
+            if (asIM == nil) {
+                return @"";
+            }
+            else{
+                
+                if(asIM.advertisingTrackingEnabled){
+                    return [asIM.advertisingIdentifier UUIDString];
+                }
+                else{
+                    return [asIM.advertisingIdentifier UUIDString];
+                }
+            }
+        }
+    }
+}
+
+- (NSString *)idfvString
+{
+    if([[UIDevice currentDevice] respondsToSelector:@selector( identifierForVendor)]) {
+        return [[UIDevice currentDevice].identifierForVendor UUIDString];
+    }
+    
+    return @"";
+}
+
+
+// umeng 渠道分析代码 end
+
+
 
 
 @end
